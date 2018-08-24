2bcba94e83ecc31af0fceff6a8809c4a8f173cef
mattbrailsford <me@mattbrailsford.com>
2018-08-24 08:09:52 +0100

    Extract styles to stylesheet

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
 import minify from 'rollup-plugin-babel-minify'; // Minify the output js
+import css from 'rollup-plugin-css-only';
 
 export default {
     input: 'src/wrapper.js', // Path relative to package.json
@@ -9,8 +10,9 @@ export default {
         exports: 'named',
     },
     plugins: [
+        css({ output: 'dist/vue-lightbox.css' }),
         vue({
-            css: 'dist/vue-litebox.css', // Export css to file
+            css: false, // Export css to file
             compileTemplate: true, // Explicitly convert template to render function
         }),
         buble(), // Transpile to ES5
